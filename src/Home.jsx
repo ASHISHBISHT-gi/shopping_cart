5b862b2007d3111b5dcd81e35faafbb617251056
ASHISHBISHT-gi <syntanalprime@gmail.com>
2026-06-19 23:49:37 +0530

    data extraction successful

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router";
+
 export default function Home(){
     return(
+    <>
       <p>i am home</p>
+      <header>
+       <h1>Shopping</h1>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/shop">Shop</Link></li>
+          <li><Link to="/cart">Cart</Link></li>
+        </ul>
+      </header>
+     </> 
     )
 }
--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,5 +1,55 @@
+import { Link } from "react-router";
+import { useEffect , useState} from "react";
+function Image(){
+  
+  
+  return(
+    <p>i am image</p>
+  )
+}
+
+
+
+
+
+
+
+
 export default function Shop(){
+  const [productData , setproductData] = useState([]);
+    // extract data from the api
+    useEffect(()=>{
+    async function getData(){
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+
+      console.log(data.length);
+      let newdata=[]
+      for(let i =0; i < data.length ; i++){
+          newdata.push(data[i].image);
+        console.log(data[i])
+      }
+      console.log("newdata is ",newdata)
+      setproductData(newdata);
+    }
+    getData();
+  },[])
     return(
-    <p>i am shop</p>
+    <>
+      <p>i am shop</p>
+      <header>
+        <h1>Shopping</h1>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/shop">Shop</Link></li>
+          <li><Link to="/cart">Cart</Link></li>
+        </ul>
+      </header>
+      <div className="shop">
+        <Image />
+      </div>
+
+    </>
     )
 }
+
